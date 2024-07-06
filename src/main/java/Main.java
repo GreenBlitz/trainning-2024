3b8f5a0d10a130bb19c6680c7a5392c8fd1dc738
YHerm <0549405422.yh@gmail.com>
2024-07-06 19:30:21 +0300

    yoav: format

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,14 +9,16 @@
  * call.
  */
 public final class Main {
-  private Main() {}
 
-  /**
-   * Main initialization function. Do not perform any initialization here.
-   *
-   * <p>If you change your main robot class, change the parameter type.
-   */
-  public static void main(String... args) {
+    private Main() {}
 
-  }
+    /**
+     * Main initialization function. Do not perform any initialization here.
+     *
+     * <p>If you change your main robot class, change the parameter type.
+     */
+    public static void main(String... args) {
+
+    }
+
 }
