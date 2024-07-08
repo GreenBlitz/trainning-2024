d0245967ab1c29ac771afcdd915ac503a494aff2
GreenBlitz <greenblitz4590@gmail.com>
2024-07-08 10:58:31 +0300

    Yoni - opened branch

--- src/main/java/training/TrainingRobotManager.java
+++ src/main/java/training/TrainingRobotManager.java
@@ -1,14 +1,18 @@
 package training;
 
+import com.ctre.phoenix.motorcontrol.IMotorController;
+import com.ctre.phoenix6.hardware.TalonFX;
 import utils.DefaultRobotManager;
 
 public class TrainingRobotManager extends DefaultRobotManager {
 
     private Robot robot;
+    private TalonFX motor;
 
     @Override
     public void trainingInit() {
         this.robot = new Robot();
+        motor = new TalonFX(0);
     }
 
     @Override
@@ -19,12 +23,14 @@ public class TrainingRobotManager extends DefaultRobotManager {
     @Override
     public void teleopInit() {
         // schedule your command...
+        motor.set(0.5);
     }
 
 
     @Override
     public void teleopExit() {
         // cancel your command...
+        motor.set(0);
     }
 
 }
