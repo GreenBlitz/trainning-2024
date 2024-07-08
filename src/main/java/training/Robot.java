519a877600d8cba477ea6538e10ab1597b24f0db
unknown <greenblitz@gmail.com>
2024-07-08 12:04:39 +0300

    Yotams- Added move forward command, made robot class have static ModuleSubsystem property

--- src/main/java/training/Robot.java
+++ src/main/java/training/Robot.java
@@ -1,13 +1,20 @@
 package training;
 
+import training.subsystems.movement.ModuleSubsystem;
+
 public class Robot {
 
     // Enter your subsystems...
+    private static ModuleSubsystem moduleSubsystem;
 
     public Robot(){
         // Boot your subsystems...
+        moduleSubsystem = new ModuleSubsystem(0, 1);
     }
 
     // Add your subsystems getters...
+    public static ModuleSubsystem getModuleSubsystem() {
+        return moduleSubsystem;
+    }
 
 }
---
+++ src/main/java/training/commands/MoveForwardCommand.java
@@ -0,0 +1,27 @@
+package training.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import training.Robot;
+import training.subsystems.movement.ModuleSubsystem;
+
+public class MoveForwardCommand extends Command {
+    private double speed;
+    private ModuleSubsystem subsystem;
+
+    public MoveForwardCommand(double speed) {
+        this.speed = speed;
+        this.subsystem = Robot.getModuleSubsystem();
+        this.addRequirements(subsystem);
+    }
+
+    @Override
+    public void initialize() {
+        subsystem.setAngularSpeed(0);
+        subsystem.setLinearSpeed(speed);
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        subsystem.stopMotor();
+    }
+}
