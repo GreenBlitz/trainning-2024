0291c9824317c1be9078bd95f3c92899598ec95c
unknown <greenblitz@gmail.com>
2024-07-08 11:41:41 +0300

    Yotams- add Module subsystem

--- src/main/java/training/subsystems/movement/Constants.java
+++ src/main/java/training/subsystems/movement/Constants.java
@@ -1,2 +1,5 @@
-package training.subsystems.movement;public class Constants {
+package training.subsystems.movement;
+
+public class Constants {
+    protected static final double MAX_ALLOWED_SPEED=0.9;
 }
--- src/main/java/training/subsystems/movement/ModuleSubsystem.java
+++ src/main/java/training/subsystems/movement/ModuleSubsystem.java
@@ -1,2 +1,42 @@
-package training.subsystems;public class ModuleSubsystem {
+package training.subsystems.movement;
+
+import com.ctre.phoenix6.hardware.TalonFX;
+import utils.GBSubsystem;
+
+
+public class ModuleSubsystem extends GBSubsystem {
+    private final TalonFX linearMotor;
+    private final TalonFX angularMotor;
+
+    public ModuleSubsystem(int linearMotorId, int rotationalMotorId) {
+        linearMotor = new TalonFX(linearMotorId);
+        angularMotor = new TalonFX(rotationalMotorId);
+    }
+
+    public void setLinearSpeed(double speed) {
+        if (Math.abs(speed) <= Constants.MAX_ALLOWED_SPEED) {
+            linearMotor.set(speed);
+        }
+    }
+
+    public void setAngularSpeed(double speed) {
+        if (Math.abs(speed) <= Constants.MAX_ALLOWED_SPEED) {
+            angularMotor.set(speed);
+        }
+    }
+
+    public void stopMotor(){
+        setLinearSpeed(0);
+        setAngularSpeed(0);
+    }
+
+    @Override
+    protected String getLogPath() {
+        return "";
+    }
+
+    @Override
+    protected void subsystemPeriodic() {
+
+    }
 }
