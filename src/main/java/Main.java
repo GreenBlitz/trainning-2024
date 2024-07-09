00e016634029b22f49239f4726714d9cd47a231e
Shahar Perets <perets.shahar@gmail.com>
2024-07-09 17:29:13 +0300

    Shahar P.: this is my work from previous 2 days that is unrelated to the swerve module. Finnaly  is working correctly.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,11 @@
 // the WPILib BSD license file in the root directory of this project.
 
 
+import java.util.Random;
+import java.util.Scanner;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
 /**
  * Do NOT add any static variables to this class, or any initialization at all. Unless you know what
  * you are doing, do not modify this file except to change the parameter class to the startRobot
@@ -14,6 +19,8 @@ public final class Main {
 
     public static void main(String... args) {
         System.out.println(findOptimalBankToRobber());
+        SuppliersAndCunsumers();
+        exceptionsTryAndCatch();
     }
 
     public static Bank findOptimalBankToRobber() {
@@ -27,5 +34,29 @@ public final class Main {
         }
 
         return output;
+    }
+    public static void SuppliersAndCunsumers() {
+        Random rand = new Random();
+        Supplier<Integer> getRandomNumber = () -> rand.nextInt(1, 42);
+        Consumer<Integer> printInteger = (Integer Something) -> System.out.println(Something);
+
+        printInteger.accept(getRandomNumber.get());
+    }
+
+    public static void exceptionsTryAndCatch() throws IllegalArgumentException {
+        Scanner scanner = new Scanner(System.in);
+        while(true) {
+            int input = scanner.nextInt();
+            if (input == 45) {
+                try {
+                    throw new IllegalArgumentException();
+                } catch (IllegalArgumentException e) {
+                    // Do nothing
+                }
+            }
+            if (input == 100) {
+                throw new RuntimeException();
+            }
+        }
     }
 }
