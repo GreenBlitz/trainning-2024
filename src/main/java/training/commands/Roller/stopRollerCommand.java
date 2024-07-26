d9f3499d90bf0abfc5f4379a650f3748fbda65e6
Shahar Perets <perets.shahar@gmail.com>
2024-07-26 17:33:39 +0300

    the stopRollerCommand should end now

--- src/main/java/training/commands/Roller/stopRollerCommand.java
+++ src/main/java/training/commands/Roller/stopRollerCommand.java
@@ -1,5 +1,6 @@
 package training.commands.Roller;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import training.subsystems.Roller.Roller;
 
@@ -15,4 +16,15 @@ public class stopRollerCommand extends Command {
         addRequirements(roller);
         roller.stop();
     }
+
+    @Override
+    public boolean isFinished() {
+        try {
+            wait(5);
+        } catch (InterruptedException e) {
+            SmartDashboard.putString("got error on stopRollerCommand InterruptedException: ", e.getMessage());
+        }
+        return true;
+    }
+
 }
