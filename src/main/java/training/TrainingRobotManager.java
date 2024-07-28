4dc15acdae00c9d1ddc065b88628a559505f7a31
Maya Romm <maya.romm0401@gmail.com>
2024-07-28 16:21:03 +0300

    Romy - initial

--- src/main/java/training/TrainingRobotManager.java
+++ src/main/java/training/TrainingRobotManager.java
@@ -1,30 +1,17 @@
 package training;
 
-import edu.wpi.first.math.geometry.Rotation2d;
-import training.commands.GoToPosition;
-import training.commands.MoveModuleMotor;
-import training.commands.MoveMotorWithJoystick;
 import utils.DefaultRobotManager;
 import utils.joysticks.SmartJoystick;
 
 public class TrainingRobotManager extends DefaultRobotManager {
 
     private Robot robot;
-    private MoveModuleMotor moveModule;
-    private MoveMotorWithJoystick moveMotorWithJoystick;
     private SmartJoystick joystick;
-    private GoToPosition goToPosition;
 
     @Override
     public void trainingInit() {
         robot = new Robot();
-        moveModule = new MoveModuleMotor(robot.getModule());
         joystick = new SmartJoystick(RobotConstants.ID_OF_FIRST_JOYSTICK);
-        joystick.A.whileTrue(moveModule);
-        moveMotorWithJoystick = new MoveMotorWithJoystick(robot, joystick);
-        goToPosition = new GoToPosition(robot, Rotation2d.fromDegrees(200));
-        robot.getModule().setDefaultCommand(moveMotorWithJoystick);
-        joystick.B.onTrue(goToPosition);
     }
 
     @Override
