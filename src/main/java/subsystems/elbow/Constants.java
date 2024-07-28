05483935e443dcc3a2ebb4a58c656a27f2feba65
Yotam7 <76944434+Yotam7@users.noreply.github.com>
2024-07-28 18:21:08 +0300

    Yotam- switch the elbow subsystem to use fit forward

--- src/main/java/subsystems/elbow/Constants.java
+++ src/main/java/subsystems/elbow/Constants.java
@@ -2,12 +2,18 @@ package subsystems.elbow;
 
 import com.revrobotics.CANSparkBase;
 import com.revrobotics.CANSparkLowLevel;
-import com.revrobotics.CANSparkMax;
+import edu.wpi.first.math.controller.ArmFeedforward;
 
 public class Constants {
 
     static final int ELBOW_MOTOR_ID = 0; // TODO replace with actual value
     static final CANSparkLowLevel.MotorType ELBOW_MOTOR_TYPE = CANSparkLowLevel.MotorType.kBrushless;
     static final CANSparkBase.ControlType ELBOW_CONTORL_TYPE = CANSparkBase.ControlType.kPosition;
+
+    static final int SINGLE_ROTATION=1;
+
+    static final ArmFeedforward ELBOW_FIT_ARM = new ArmFeedforward(0,0,0); // TODO replace with useful FF values
+    static final int WRIST_PID_SLOT =0;
+
 
 }
--- src/main/java/subsystems/elbow/Elbow.java
+++ src/main/java/subsystems/elbow/Elbow.java
@@ -1,6 +1,5 @@
 package subsystems.elbow;
 
-import com.ctre.phoenix6.hardware.TalonFX;
 import com.revrobotics.CANSparkMax;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -20,7 +19,19 @@ public class Elbow extends SubsystemBase {
         return instance;
     }
 
+    public Rotation2d getAngle(){
+        return Rotation2d.fromRotations(motor.getEncoder().getPosition()%Constants.SINGLE_ROTATION);
+    }
+
+    public double getRPMVelocity(){
+        return motor.getEncoder().getVelocity();
+    }
+
+    private double calculateFitForward(){
+        return Constants.ELBOW_FIT_ARM.calculate(getAngle().getRadians(),getRPMVelocity());
+    }
+
     public void getToPosition(Rotation2d position){
-        motor.getPIDController().setReference(position.getRadians(), Constants.ELBOW_CONTORL_TYPE);
+        motor.getPIDController().setReference(position.getRadians(), Constants.ELBOW_CONTORL_TYPE,Constants.WRIST_PID_SLOT ,calculateFitForward());
     }
 }
