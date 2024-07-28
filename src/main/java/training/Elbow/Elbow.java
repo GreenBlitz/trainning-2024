cf47da0d48aaaadcecd88908e6e8a55e02d6302c
Shahar Perets <perets.shahar@gmail.com>
2024-07-28 20:52:07 +0300

    Shahar: deg - rad is a bad idea

--- src/main/java/training/Elbow/Elbow.java
+++ src/main/java/training/Elbow/Elbow.java
@@ -56,7 +56,7 @@ public class Elbow extends GBSubsystem {
     }
 
     public boolean isAtAngle(Rotation2d angle) {
-        double anglesDelta = (this.targetAngle.getDegrees() % 1) - (angle.getRadians() % 1);
+        double anglesDelta = (this.targetAngle.getDegrees() % 360) - (angle.getDegrees() % 360);
         return Math.abs(anglesDelta) <= ELBOW_TOLERANCE.getRadians();
     }
 
