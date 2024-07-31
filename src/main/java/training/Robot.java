c718fc5318929de1fdbe6a233ce9429aba1bb9d7
Yotam7 <76944434+Yotam7@users.noreply.github.com>
2024-07-31 18:35:04 +0300

    boot subsytems

--- src/main/java/training/Robot.java
+++ src/main/java/training/Robot.java
@@ -1,11 +1,21 @@
 package training;
 
+import subsystems.Roller.Roller;
+import subsystems.elbow.Elbow;
+import subsystems.wrist.Wrist;
+
 public class Robot {
 
     // Enter your subsystems...
+    private final Elbow elbow;
+    private final Wrist wrist;
+    private final Roller roller;
 
     public Robot(){
         // Boot your subsystems...
+        elbow = Elbow.getInstance();
+        wrist = Wrist.getInstance();
+        roller = Roller.getInstance();
     }
 
     // Add your subsystems getters...
