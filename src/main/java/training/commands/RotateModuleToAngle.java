04987d198850f611679a50ee26f32679f4fdbdef
Maya Romm <maya.romm0401@gmail.com>
2024-07-31 19:44:49 +0300

    maya-add-command

---
+++ src/main/java/training/commands/RotateModuleToAngle.java
@@ -0,0 +1,23 @@
+package training.commands;
+
+import training.subsystems.SwerveModule;
+import utils.GBCommand;
+import utils.GBSubsystem;
+
+public class RotateModuleToAngle extends SwerveModuleCommand {
+    @Override
+    public void execute() {
+        SwerveModule.getInstance().setVoltageAngular(1);
+    }
+
+    @Override
+    public boolean isFinished(){
+        return SwerveModule.getInstance().isItAtAngle(SwerveModule.getInstance().getAngle());
+    }
+
+    @Override
+    public void end(boolean interrupted){
+        SwerveModule.getInstance().stop();
+    }
+
+}
