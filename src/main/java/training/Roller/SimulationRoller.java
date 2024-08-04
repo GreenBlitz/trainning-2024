52218c2cfcc2a11447ce84fa1e30108836c461df
Shahar Perets <perets.shahar@gmail.com>
2024-08-04 20:48:53 +0300

    Shahar: subtle changes for the roller

--- src/main/java/training/Roller/SimulationRoller.java
+++ src/main/java/training/Roller/SimulationRoller.java
@@ -9,19 +9,11 @@ import static training.Roller.RollerConstants.*;
 import static training.Roller.RollerDirection.kBackward;
 import static training.Roller.RollerDirection.kForward;
 
-public class SimulationRoller extends GBSubsystem implements IRoller {
+public class SimulationRoller implements IRoller {
     private final DCMotorSim motor;
-    private double targetVelocity;
-    private RollerDirection direction;
 
     public SimulationRoller() {
         this.motor = new DCMotorSim(DCMotor.getNEO(1), ROLLER_GEARING, POWER_TO_ACCELERATION);
-        this.targetVelocity = ROLLER_DEFAULT_VELOCITY_RPM;
-    }
-
-    @Override
-    protected String getLogPath() {
-        return ROLLER_LOG_PATH;
     }
 
     @Override
@@ -31,17 +23,17 @@ public class SimulationRoller extends GBSubsystem implements IRoller {
 
     @Override
     public Rotation2d getPosition() {
-        return null;
+        return Rotation2d.fromRotations(motor.getAngularPositionRotations());
     }
 
     @Override
     public Rotation2d getVelocity() {
-        return null;
+        return Rotation2d.fromRotations(motor.getAngularPositionRotations());
     }
 
     @Override
     public void updateVelocity(Rotation2d targetVelocity, RollerDirection direction) {
         ROLLER_SIMULATION_CONTROLLER.setSetpoint(targetVelocity.times(direction.toInt()).getRadians());
-        motor.setInputVoltage(ROLLER_SIMULATION_CONTROLLER.calculate(motor.getAngularPositionRad(), motor.getAngularVelocityRadPerSec()));
+        setPower(ROLLER_SIMULATION_CONTROLLER.calculate(motor.getAngularPositionRad(), motor.getAngularVelocityRadPerSec()));
     }
 }
