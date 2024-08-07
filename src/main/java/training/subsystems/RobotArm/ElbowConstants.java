37b8586866d717187610355b4d2bf2215b16ad85
tomer-hershman <lili.hershman@gmail.com>
2024-08-07 16:23:53 +0300

    cr 1

---
+++ src/main/java/training/subsystems/RobotArm/ElbowConstants.java
@@ -0,0 +1,16 @@
+package training.subsystems.RobotArm;
+
+public class ElbowConstants {
+    public static final double BIGINNING_POSITION = 0,
+            KP = 20,
+            KI = 0,
+            KD = 0,
+            KS = 0,
+            KA = 0,
+            KV = 0,
+            KG = 0,
+            ARE_FEED_FORWARDS = 0;
+    public static final int MOTOR_ID = 0,
+            PID_SLOT = 0;
+
+}
