b93d676a49915cf078cb21cc5f9fbb65f3e5a103
Shahar Perets <perets.shahar@gmail.com>
2024-08-18 19:03:37 +0300

    Shahar: simulation but better

--- src/main/java/training/Elbow/SimulationElbow/SimulationElbow.java
+++ src/main/java/training/Elbow/SimulationElbow/SimulationElbow.java
@@ -1,13 +1,13 @@
 package training.Elbow.SimulationElbow;
 
-import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.DifferentialPositionVoltage;
+import com.ctre.phoenix6.controls.DutyCycleOut;
+import com.ctre.phoenix6.controls.PositionVoltage;
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.wpilibj.simulation.SingleJointedArmSim;
 import org.littletonrobotics.junction.Logger;
 import training.Elbow.ElbowInputsAutoLogged;
 import training.Elbow.IElbow;
 import utils.simulation.SimpleMotorSimulation;
-import utils.simulation.SingleJointedArmSimulation;
 
 public class SimulationElbow implements IElbow {
 
@@ -16,34 +16,13 @@ public class SimulationElbow implements IElbow {
 
 	public SimulationElbow() {
 		this.arm = new SimpleMotorSimulation();
-//		this.arm = new SingleJointedArmSimulation(
-//			new SingleJointedArmSim(
-//				SimulationElbowConstants.GEARBOX,
-//				SimulationElbowConstants.GEARING,
-//				SingleJointedArmSim.estimateMOI(SimulationElbowConstants.LENGTH_METERS, SimulationElbowConstants.MASS),
-//				SimulationElbowConstants.LENGTH_METERS,
-//				SimulationElbowConstants.MINIMUM_ANGLE.getRadians(),
-//				SimulationElbowConstants.MAXIMUM_ANGLE.getRadians(),
-//				true,
-//				0
-//			)
-//		);
-		arm.applyConfiguration(SimulationElbowConstants.config);
+		arm.applyConfiguration(SimulationElbowConstants.CONFIG);
 
 	}
 
 	@Override
 	public void moveToAngle(Rotation2d targetAngle) {
-		double feedForwardOutputVoltage = SimulationElbowConstants.FEEDFORWARD
-			.calculate(arm.getPosition().getRadians(), arm.getVelocity().getRadians());
-		double PIDOutputVoltage = SimulationElbowConstants.CONTROLLER.calculate(arm.getPosition().getRadians(), targetAngle.getRadians());
-
-		Logger.recordOutput("Elbow/FF out: ", Rotation2d.fromRadians(feedForwardOutputVoltage));
-		Logger.recordOutput("Elbow/PID out: ", Rotation2d.fromRadians(PIDOutputVoltage));
-
-		arm.setPower(
-				PIDOutputVoltage + feedForwardOutputVoltage
-		);
+		arm.setControl(new PositionVoltage(targetAngle.getRotations()));
 	}
 
 	@Override
--- src/main/java/training/Elbow/SimulationElbow/SimulationElbowConstants.java
+++ src/main/java/training/Elbow/SimulationElbow/SimulationElbowConstants.java
@@ -1,7 +1,6 @@
 package training.Elbow.SimulationElbow;
 
 import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
-import com.ctre.phoenix6.configs.MotorOutputConfigs;
 import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import edu.wpi.first.math.controller.ArmFeedforward;
@@ -11,35 +10,24 @@ import edu.wpi.first.math.system.plant.DCMotor;
 
 public class SimulationElbowConstants {
 
-	protected static final DCMotor GEARBOX = DCMotor.getNEO(1);
-
-	protected static final double GEARING = 28.0 * (60.0 / 16.0);
-
-	protected static final double LENGTH_METERS = 0.44;
-
-	protected static final double MASS = 0.44;
-
-	protected static final Rotation2d MINIMUM_ANGLE = Rotation2d.fromRotations(-0.5);
-
-	protected static final Rotation2d MAXIMUM_ANGLE = Rotation2d.fromRotations(0.5);
-
 	protected static final PIDController CONTROLLER = new PIDController(3, 0, 1); // Outputs power
 
 	protected static final ArmFeedforward FEEDFORWARD = new ArmFeedforward(0, 12, 0, 0); // Keep kV and kA 0
 
-	protected static final TalonFXConfiguration config = new TalonFXConfiguration();
+	protected static final TalonFXConfiguration CONFIG = new TalonFXConfiguration();
 
 	static {
-		config.withSlot0(new Slot0Configs()
-				.withKP(4)
+		CONFIG.withSlot0(new Slot0Configs()
+				.withKP(0.8)
 				.withKD(0)
 				.withKI(0)
-				.withKG(12)
+				.withKG(0)
 				.withKS(0)
 				.withKA(0)
 				.withKV(0)
 		);
-		config.withCurrentLimits(new CurrentLimitsConfigs());
+		CONFIG.withCurrentLimits(new CurrentLimitsConfigs());
+		CONFIG.Feedback.RotorToSensorRatio = 28.0 * (60.0 / 16.0);
 	}
 
 }
